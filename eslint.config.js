a114fb8618e2f6211bef5d3f3c56a71e12b0ba46
Vijay-Psgtech <rvk.its@psgtech.ac.in>
2026-09-10 09:49:23 +0530

    Git ignore , config & vite config js changes & updateded code has been added.

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,40 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+  js.configs.recommended,
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
-      globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooks,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
     },
   },
-])
+];
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,29 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import tailwindcss from 'tailwindcss' 
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+const apiTarget = process.env.VITE_API_TARGET || 'http://localhost:5010';
+const assetDetailPath = /^\/assets\/[a-f\d]{24}$/i;
+
+const serveAssetDetailRoute = (server) => {
+  server.middlewares.use((request, _response, next) => {
+    if (request.method === 'GET' && assetDetailPath.test(request.url || '')) {
+      request.url = '/index.html';
+    }
+    next();
+  });
+};
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+  plugins: [react()],
+  configureServer: serveAssetDetailRoute,
+  configurePreviewServer: serveAssetDetailRoute,
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: apiTarget,
+        changeOrigin: true,
+      },
+    },
+  },
+});
